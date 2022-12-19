c136740318c0abedaebd1247c2a9d9a237ef2b5c
joshuarbruce <joshrbruce@gmail.com>
2022-12-19 12:26:56 -0600

    Update library.js

--- js/library.js
+++ js/library.js
@@ -45,6 +45,21 @@ function create_colorSwatch() {
 
 }
 
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
 // - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
